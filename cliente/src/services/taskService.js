b96188992da629332f0b98d4f254a61fdfdb7e99
Yan Lagarejo <clagarejo89@gmail.com>
2025-01-15 22:47:44 -0500

    deploy

--- cliente/src/services/taskService.js
+++ cliente/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:3001/tasks";
+const API_URL = "https://server-task.fly.dev/tasks";
 
 export const getTasks = () => axios.get(API_URL);
 export const addTask = (task) => axios.post(API_URL, task);
